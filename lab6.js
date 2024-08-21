87b56f09fbb0804d7c33d5b202f1a39f1b6ddf04
WaseemUddinSecA <n01583077@humbermail.ca>
2024-08-21 17:33:21 -0400

    added a js file

---
+++ lab6.js
@@ -0,0 +1,30 @@
+// Task 1: 
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+console.log(myNumber);
+console.log(myString);
+// Task 2
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(3 * i);
+}
+
+// Task 3
+
+function addNumbers(num1, num2) {
+ 
+    return num1 + num2;
+}
+
+console.log(addNumbers(5, 7));
+
+function multiplyNumbers(num1, num2) {
+
+    return num1 * num2;
+}
+
+console.log(multiplyNumbers(3, 4));
